f8ac6964376d65fa5af878b6eef75cd2b03a574e
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-09-11 17:46:23 -0700

    a program that prints the lowercase alphabet in reverse

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- *  * main - prints all single digit numbers of base
- *  * 10 starting from 0, followed by a new line
- *  * Return: Always 0 (Success)
+ * main - print if the number is postive, zero, or negative
+ *   * Description: using the main function
+ *   * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
 int n;
 for (n = 0; n < 10; n++)
+{
 	printf("%d", n);
+}
 	printf("\n");
 return (0);
 }
