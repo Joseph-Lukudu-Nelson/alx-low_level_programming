33fa8505444cf37581030c27429dcc0e47aae5e9
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-09-19 13:09:46 -0700

    h fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	length1 = 0;
 	length2 = 0;
 
-	while  (s[length1] != ' \ 0 ')
+	while  (s[length1] != '\0')
 		lenght1++;
 	length2 = length1 - 1;
 		for (i = 0; i < length1 / 2; i++)
