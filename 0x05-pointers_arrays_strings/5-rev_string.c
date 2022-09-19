c1ca194634397762f1209bb4fe8b1dd6dfe74159
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-09-19 12:41:54 -0700

    a function that reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * rev_string -> reversing a string
+ * @s: parameter s
+ */
+
+void rev_string(char *s)
+{
+	"jose" - > "esoj"
+	char tmp;
+	int i, length1, lenght2;
+
+	lenght1 = 0;
+	lenght2 = 0;
+
+	while  (s[lenght1] != ' \ 0 ')
+		lenght1++;
+	lenght2 = lenght1 - 1
+		for (i = 0; i < lenght1 / 2; i++)
+		{
+			tmp = s[i];
+			s[i] = s[lenght2];
+			s[lenght2] tmp;
+			lenght2 -= 1;
+		}
+}
