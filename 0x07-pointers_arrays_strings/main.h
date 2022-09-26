7616f357997d47e5540212adc52769ba2dd8f7b5
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-09-26 11:45:43 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototypes()
- * int prototypes()
- */
+#include <stdio.h>
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -16,4 +14,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#define /*MAIN_H*/
+#endif
