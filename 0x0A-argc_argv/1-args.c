b098d7755d71d23a3e4531ca42dfdd3e1501be20
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-10-04 06:52:17 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[1])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argv[])
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
