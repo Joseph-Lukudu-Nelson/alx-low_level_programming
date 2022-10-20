1c794a099a724e6ad7f4bd6d6bce3623294e17e1
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-10-20 09:45:31 -0700

    fixed 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void main_constructor(void) __attribute__((constructor));
  */
 void main_constructor(void)
 {
-  printf("You're beat! and yet, you must allow,\n");
-  printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
